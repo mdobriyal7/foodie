9e1e15c9934563b912a6bfe3aafe0d62523b45b3
Manish <mdobriyal7@gmail.com>
2023-07-15 08:41:51 +0530

    minor change

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from "react";
-import { BrowserRouter as Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Dashboard, { DashboardRoutes } from "./components/Dashboard/Dashboard";
 import Layout from "./components/Layout";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-
 function App() {
   return (
     <>
-    <ToastContainer/>
-    <Routes>
-      <Route path="/" element={<Layout />}>
-      <Route element={<Dashboard/>} />
-      <Route path="dashboard/*" element={<DashboardRoutes/>} />
+      <ToastContainer />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="dashboard/*" element={<DashboardRoutes />} />
         </Route>
-     
       </Routes>
-      </>
+    </>
   );
 }
 
